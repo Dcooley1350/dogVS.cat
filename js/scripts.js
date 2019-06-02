3ea446eaca2f9db382cc5159c374254d140795f5
Devin Cooley <dcooley1350@gmail.com>
2019-06-01 17:33:15 -0700

    add final button to pop up image and styling.

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,8 @@ $(document).ready(function() {
   $("button#bark").click(function() {
     $("ul#dogBark").prepend("<li>BARK!</li>");
     $("ul#catHiss").prepend("<li>HISSSSS!</li>");
-  }); 
+  });
+  $("button#goodBoy").click(function() {
+    $("ul#CatIsGone").append("<li><img src='img/goodboy.jfif' alt='A very good boy'></li>")
+  })
 })
